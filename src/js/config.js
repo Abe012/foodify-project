94b287c4f71befa7add0931b32229f7e6172fbd0
Abe012 <abeneshsekar12@gmail.com>
2024-03-29 22:02:16 +0530

    inital commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '522c6e6d-7e7b-416f-95df-8750dbeb3edc';
+export const MODAL_CLOSE_SEC = 2.5;
